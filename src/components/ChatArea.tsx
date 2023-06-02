2b18811ebadecda4c298f0cc7d29891d9ca1c964
Filipe Bacof <filipebacof@gmail.com>
2023-06-01 23:25:30 -0300

    scroll smooth

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -2,6 +2,7 @@ import { Chat } from '@/types/Chat'
 import { ChatPlaceholder } from './ChatPlaceholder'
 import { ChatMessageItem } from './ChatMessageItem'
 import { ChatMessageLoading } from './ChatMessageLoading'
+import { useEffect, useRef } from 'react'
 
 type Props = {
   chat: Chat | undefined
@@ -9,8 +10,17 @@ type Props = {
 }
 
 export const ChatArea = ({ chat, loading }: Props) => {
+  const scrollArea = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    scrollArea.current?.scrollTo({
+      top: scrollArea.current?.scrollHeight,
+      behavior: 'smooth',
+    })
+  }, [loading, chat?.messages?.length])
+
   return (
-    <section className="h-0 flex-auto overflow-y-scroll">
+    <section ref={scrollArea} className="h-0 flex-auto overflow-y-scroll">
       {!chat && <ChatPlaceholder />}
       {chat &&
         chat.messages.map((item) => (
