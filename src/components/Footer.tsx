50ab064c3ee6ea36bb0b5cf542821203242e9377
Filipe Bacof <filipebacof@gmail.com>
2023-06-04 02:18:56 -0300

    style cursor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,10 +10,10 @@ export const Footer = ({ disabled, onSendMessage }: Props) => {
     <footer className="w-full border-t border-t-gray-600 p-2">
       <div className="m-auto max-w-4xl">
         <ChatMessageInput disabled={disabled} onSend={onSendMessage} />
-        <div className="pt-3 text-center text-xs text-gray-300">
+        <div className="cursor-default pt-3 text-center text-xs text-gray-300">
           Feito por Filipe Bacof - Veja o meu portifólio:&nbsp;
           <a
-            className="underline"
+            className="cursor-pointer underline"
             target="_blank"
             href="https://portifolio-filipe-bacof.vercel.app/"
             rel="noreferrer"
